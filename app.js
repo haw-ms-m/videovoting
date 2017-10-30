af926b248a598a8dd63a00e52721dba5108bf6aa
sabrina <sabrina@hauptsache.net>
2017-10-30 22:07:59 +0100

    layout 5 rows add warning labels

--- app.js
+++ app.js
@@ -1,17 +1,12 @@
 var express = require('express');
 var path = require('path');
-var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 var flash = require('connect-flash');
 var app = express();
-var moment = require('moment');
 
 app.set('views', path.join(__dirname, 'views'));
-
-// uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 
 // view engine setup
 app.engine('.ejs', require('ejs').__express);
@@ -205,11 +200,6 @@ app.post('/bar-order', function (request, response) {
 
 
     var currentUserId = request.session.userId;
-    var date = new Date();
-    var time = date.getTime();
-
-    //var dateString = moment.unix(time).format("YYYY-MM-DD HH:MM:SS");
-    //console.log(dateString);
 
     // Zuerst Orders abfragen
     mysqlConnect.connect(function (err) {
